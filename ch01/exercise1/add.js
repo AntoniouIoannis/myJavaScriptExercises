55d9b4175aa90cba8be29cbdfb69000e24b3e17f
AntoniouIoannis <antoniouioannis10@gmail.com>
2024-07-13 01:30:52 +0300

    second try

--- ch01/exercise1/add.js
+++ ch01/exercise1/add.js
@@ -3,5 +3,9 @@ let sum = 0;
 
 sum = num1 + num2;
 
-
+console.log(" ")
+console.log("******")
 console.log(`Το άθροισμα του  ${num1}  με τον  ${num2}  είναι:  ${sum}`)
+console.log("-------")
+console.log(" ")
+
