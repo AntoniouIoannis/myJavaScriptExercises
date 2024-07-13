554deeba70bc119fdd877a7b8b88f5e0984f673a
AntoniouIoannis <antoniouioannis10@gmail.com>
2024-07-13 11:55:41 +0300

    reload

---
+++ ch01/examples/examBoolean.js
@@ -0,0 +1,11 @@
+console.log("1. Boolean(1): " + Boolean(0))
+console.log("2. Boolean(0): " + Boolean(1))
+console.log(`3. Boolean(""): ` + Boolean(""))
+console.log(`4. Boolean("Hello"): ` + Boolean(" "))
+console.log((1) ? " is true" : (0) ? "1 is true" : "0 is false")
+
+while (1) {break}
+do {console.log("Hello Coding Factory")} while (0)
+for(i=1; i; i++) {console.log("In for "); break }
+
+
---
+++ ch01/examples/helloWorld.js
@@ -0,0 +1,9 @@
+let namejohn = "Giannis"
+let hello = "Hello"
+
+console.log("****")
+console.log("hello John")
+console.log(" ")
+console.log(hello + ", " +  namejohn)
+console.log("----")
+
